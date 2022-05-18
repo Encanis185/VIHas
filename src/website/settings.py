95d1359334b453fea6e67bd220444eeed813a9ba
CristianMercado <cristian.mercado5604@alumnos.udg.mx>
2022-05-18 13:00:45 -0500

    A ver si jala el host xd

--- src/website/settings.py
+++ src/website/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t$g5c*oq2j-^tgrjvhdi@q@g)sx=9t$di*8p3mmx)_%h57r&(7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
